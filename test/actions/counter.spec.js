7bcfcb523ba54276f2aebfac9229deac56c79636
Quentin Bahers <quentin.bahers@gmail.com>
2015-11-02 15:36:31 +0000

    Test reducers and some actions

---
+++ test/actions/counter.spec.js
@@ -0,0 +1,12 @@
+import expect from 'expect'
+import * as actions from '../../actions/counter'
+
+describe('actions', () => {
+  it('increment should create increment action', () => {
+    expect(actions.increment()).toEqual({ type: actions.INCREMENT_COUNTER })
+  })
+
+  it('decrement should create decrement action', () => {
+    expect(actions.decrement()).toEqual({ type: actions.DECREMENT_COUNTER })
+  })
+})
